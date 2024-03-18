d9ac8bd4b1a9fa71b3015c80f34dddf303708d8c
ViniMata <vinicius.matareli2005@gmail.com>
2024-03-18 17:33:02 -0300

    att

--- gerador_lista.py
+++ gerador_lista.py
@@ -1,10 +1,14 @@
+import os
 list = []
+titulo_lista = input("Qual o nome da sua lista? ")
 while True:
     nome =  input("NOme:")
     if nome == "sair":
         break
     else:
         list.append(nome)
+os.system("cls")
+print(f"{titulo_lista}:\n")
 for x in list:
     new = x.capitalize()
-    print(f"-{new}")
+    print(f" -{new}")
