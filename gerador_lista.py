624ae03a3bf54029f89985b31c3b695c4092cb66
ViniMata <vinicius.matareli2005@gmail.com>
2024-03-18 19:37:10 -0300

    att

--- gerador_lista.py
+++ gerador_lista.py
@@ -1,12 +1,14 @@
 import os
 list = []
+os.system("cls")
 titulo_lista = input("Qual o nome da sua lista? ")
 while True:
-    nome =  input("NOme:")
-    if nome == "sair":
-        break
-    else:
+    nome =  input("Item da lista:")
+    input("Digite 'sair' na proxima linha para sair")
+    if nome != 'sair':
         list.append(nome)
+    elif nome == "sair":
+        break
 os.system("cls")
 print(f"{titulo_lista}:\n")
 for x in list:
