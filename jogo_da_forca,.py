d024cd284c713a1ef61263e35223d9d956cc51dc
ViniMata <vinicius.matareli2005@gmail.com>
2024-03-18 01:00:11 -0300

    att

---
+++ jogo_da_forca,.py
@@ -0,0 +1,18 @@
+import os
+def escolher_e_esconder_a_palavra():
+# escolha_a_palavra()
+    palavra_chave = input("Escreva a palavra para ser descoberta:")
+
+    # esconder_a_palavra
+    tamanho_palavra = len(palavra_chave)
+    palavra_escondida = ""
+    quantidade_de_simbolos =  tamanho_palavra* "_"
+def hub_jogo():
+    print("arroz")
+
+def main():
+    escolher_e_esconder_a_palavra()
+    hub_jogo()
+
+if __name__ == "__main__":
+    main()
